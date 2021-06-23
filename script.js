a1a69316f70e3a1345cc4be57c232636fa10121e
Sidney Robhez <sidney.robhez@gmail.com>
2021-06-23 13:06:39 -0500

    Updates in date

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const countDown = () => {
-	const countDate = new Date('June 14, 2021 00:00:00').getTime();
+	const countDate = new Date('December 31, 2021 00:00:00').getTime();
 	const now = new Date().getTime();
 	const gap = countDate - now;
 
